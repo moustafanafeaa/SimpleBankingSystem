96839b42763ec22e261025b0b3fbacd29790e745
moustafa ahmed nafea <moustafa.anafea@gmail.com>
2024-02-29 17:17:42 +0200

    replace Add project files.

---
+++ SimpleBankingSystem/Models/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBankingSystem.Models
+{
+	public class BankAccount
+	{
+		
+			public int Id { get; set; }
+
+			[MinLength(3)]
+		
+			public int Accountnumber { get; set; }
+
+			[MaxLength(100)]
+			public string OwnerName { get; set; }
+		
+			public double Balance { get; set; }
+			public List<Transaction> transactions { get; set; }
+		
+	}
+}
